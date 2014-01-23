6a8b828cc3a081b5c9676042b783c89ebb0ad5dc
Wille2 <villeahti@hotmail.com>
2014-01-23 20:33:40 +0200

    now password gotten more wisely using getpass module

--- coursera.py
+++ coursera.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os, sys, time
+import getpass
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -11,15 +12,19 @@ VIDEO_DOWN_URL = '/lecture/download.mp4?lecture_id='
 
 def authenticate(email, password):
     BROWSER.get(LOGIN_URL)
+    #TODO: wait until following elements are visible
     time.sleep(1)
     BROWSER.find_element_by_css_selector('input#signin-email').send_keys(email)
     BROWSER.find_element_by_css_selector('input#signin-password').send_keys(password + Keys.ENTER)
+    #wait until some element is visible
     time.sleep(5)
 
 def download_video(course_name, video_id):
+    #TODO: go to video page and actually click links to make sure that every video is downloaded
     video_url = CLASS_BASE_URL + course_name + VIDEO_DOWN_URL + str(video_id)
     print "downloading video from url: " + video_url
     BROWSER.get(video_url)
+    #TODO: wait until there is no .part file in the download directory
     time.sleep(5)
 
 def create_browser(courser_name):
@@ -39,8 +44,9 @@ def create_browser(courser_name):
 
 if __name__ == '__main__':
     #get parameters
-    email, password, course_name,first_video_id, last_video_id = [sys.argv[i] for i in range(1,6)]
-    print email, password, course_name, last_video_id
+    email, course_name,first_video_id, last_video_id = [sys.argv[i] for i in range(1,5)]
+    password = getpass.getpass()
+    print email, course_name, last_video_id
     
     BROWSER = create_browser(course_name)
     authenticate(email,password)
@@ -49,7 +55,8 @@ if __name__ == '__main__':
             download_video(course_name, video_id)
     else:
         print "authentication failed. url was: " + BROWSER.current_url
-
+        #more error handling!!
+    #TODO: sign out and close browser when all downloads are complete
     #BROWSER.quit()
 
 
